3b825c176edcd96ba63a27be245a579066ac68cb
zEleceed <133176949+zEleceed@users.noreply.github.com>
2023-11-15 23:35:46 +0900

    11/15 Tackling recursion problems isn't fun

--- part11-17_bosses_and_subordinates/src/bosses_and_subordinates.py
+++ part11-17_bosses_and_subordinates/src/bosses_and_subordinates.py
@@ -6,3 +6,34 @@ class Employee:
 
     def add_subordinate(self, employee: 'Employee'):
         self.subordinates.append(employee)
+
+
+def count_subordinates(employee: Employee):
+    total_sub = 0
+    if len(employee.subordinates) == 0:
+        return total_sub
+    elif len(employee.subordinates) > 0:
+        for person in employee.subordinates:
+
+            total_sub += 1 + count_subordinates(person)
+    return total_sub
+
+
+
+
+
+if __name__ == "__main__":
+    t1 = Employee("Sally")
+    t2 = Employee("Eric")
+    t3 = Employee("Matthew")
+    t4 = Employee("Emily")
+    t5 = Employee("Adele")
+    t6 = Employee("Claire")
+    t1.add_subordinate(t4)
+    t1.add_subordinate(t6)
+    t4.add_subordinate(t2)
+    t4.add_subordinate(t3)
+    t4.add_subordinate(t5)
+    print(count_subordinates(t1))
+    print(count_subordinates(t4))
+    print(count_subordinates(t5))
