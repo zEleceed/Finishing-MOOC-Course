bddba1afb1c8ad1439bf7c31bd7ae95318d4c9ab
zEleceed <133176949+zEleceed@users.noreply.github.com>
2023-11-27 22:25:44 +0900

    11/27 functions as arguments practice

--- part12-05_climbing_areas/src/climbing_areas.py
+++ part12-05_climbing_areas/src/climbing_areas.py
@@ -7,6 +7,7 @@ class ClimbingRoute:
     def __str__(self):
         return f"{self.name}, length {self.length} metres, grade {self.grade}"
 
+
 class ClimbingArea:
     def __init__(self, name: str):
         self.name = name
@@ -30,3 +31,19 @@ class ClimbingArea:
         hardest_route = self.hardest_route()
         return f"{self.name} {self.routes()} routes, hardest {hardest_route.grade}"
 
+
+def sort_by_number_of_routes(grawr):
+    def amount_of_routes(x: ClimbingArea):
+        return x.routes()
+
+    return sorted(grawr, key=amount_of_routes)
+
+
+def sort_by_most_difficult(areas):
+    def most_difficult_grade(area: ClimbingArea):
+        grades_order = ['4', '4+', '5', '5+', '6A', '6A+', '6B', '6B+', '6C', '6C+', '7A', '7A+', '7B', '7B+', '7C',
+                        '7C+', '8A', '8A+', '8B', '8B+', '8C', '8C+']
+        hardest_route = area.hardest_route()
+        return grades_order.index(hardest_route.grade)
+
+    return sorted(areas, key=most_difficult_grade, reverse=True)
