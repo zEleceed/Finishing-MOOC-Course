be709630c3a382af29b9ceb472f41242ee492607
zEleceed <133176949+zEleceed@users.noreply.github.com>
2023-11-29 11:45:41 +0900

    11/29

--- part12-13_credits/src/credits.py
+++ part12-13_credits/src/credits.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
+
 class CourseAttempt:
     def __init__(self, course_name: str, grade: int, credits: int):
         self.course_name = course_name
@@ -9,4 +10,27 @@ class CourseAttempt:
     def __str__(self):
         return f"{self.course_name} ({self.credits} cr) grade {self.grade}"
 
+
 # Write your solution
+
+
+def sum_of_all_credits(courses: list):
+    return reduce(lambda reduced_total, score: reduced_total + score.credits, courses, 0)
+
+
+def sum_of_passed_credits(courses: list):
+    filtered_list = list(filter(lambda score: score.grade > 1, courses))
+
+    return reduce(lambda reduced_total, total_grades: reduced_total + total_grades.credits, filtered_list, 0)
+
+
+def average(course:list):
+    filtered_list = list(filter(lambda score: score.grade > 1, course))
+    return (reduce(lambda reduced_total, total_Grades: reduced_total + total_Grades.grade, filtered_list, 0))/len(filtered_list)
+
+
+s1 = CourseAttempt("Introduction to Programming", 5, 5)
+s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
+s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+ag = average([s1, s2, s3])
+print(ag)
--- part12-14_regular_expressions/src/regular_expressions.py
+++ part12-14_regular_expressions/src/regular_expressions.py
@@ -1,2 +1,16 @@
-# Write your solution here
 import re
+
+
+def is_dotw(my_string: str) -> bool:
+    pattern = r'\b(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\b'
+    return re.search(pattern, my_string) is not None
+
+
+def all_vowels(my_string: str) -> bool:
+    pattern = r'^[aeiouAEIOU]+$'
+    return re.fullmatch(pattern, my_string) is not None
+
+
+def time_of_day(my_string: str) -> bool:
+    pattern = r'^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$'
+    return re.fullmatch(pattern, my_string) is not None
