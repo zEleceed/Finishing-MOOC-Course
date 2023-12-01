6b27eda8a3d259a264c52d20cbd3d62a5a5a3666
zEleceed <133176949+zEleceed@users.noreply.github.com>
2023-12-01 21:00:21 +0900

    Finished Part 12!!!

--- part12-15_hockey_statistics/src/hockey_statistics.py
+++ part12-15_hockey_statistics/src/hockey_statistics.py
@@ -2,7 +2,7 @@ import json
 
 
 def formatted_string(player: dict):
-    final_string = (f"{player['name']: <21}{player['team']}{player['goals']:>3} +"
+    final_string = (f"{player['name']: <21}{player['team']} {player['goals']:>3} +"
                     f"{player['assists']:>3} = {player['goals'] + player['assists']:>3}")
     return final_string
 
@@ -16,7 +16,7 @@ def read_data(filename):
 def search_for_player(data, player_name):
     found = None
     for i in data:
-        if i['name'] == player_name:  # Compare the 'name' key to player_name
+        if i['name'] == player_name:
             found = i
             break  # Break the loop if the player is found
     if found is not None:
@@ -39,6 +39,28 @@ def list_players_by_points(data, team):
         if player['team'] == team:
             found.append(player)
     return sorted(found, key=lambda player_total: player_total["goals"] + player_total['assists'], reverse=True)
+
+
+def list_players_by_country(data, country):
+    found = [person_in_country for person_in_country in data if person_in_country['nationality'] == country]
+    return sorted(found, key=lambda player_total: player_total["goals"] + player_total['assists'], reverse=True)
+
+
+def most_points(data, player_range):
+    def player_total_score(player: dict):
+        return (player["goals"] + player["assists"]), player["goals"]
+
+    found = sorted(data, key=lambda x: player_total_score(x), reverse=True)
+    return found[:player_range]
+
+
+def most_goals(data, player_range):
+    def total_goals(player: dict):
+        return player['goals'], (player['games'])*(-1)
+
+    player_list = sorted(data, key=lambda x: total_goals(x), reverse=True)
+
+    return player_list[:player_range]
 
 
 def execute():
@@ -80,6 +102,21 @@ def execute():
             needed_list = list_players_by_points(player_Data, team)
             for name in needed_list:
                 print(formatted_string(name))
+        elif command_input == 5:
+            country = input("country: ")
+            country_players = list_players_by_country(player_Data, country)
+            for name in country_players:
+                print(formatted_string(name))
+        elif command_input == 6:
+            player_range = int(input("how many: "))
+            for i in most_points(player_Data, player_range):
+                print(formatted_string(i))
+            pass
+        elif command_input == 7:
+            player_range = int(input("how many: "))
+            for i in most_goals(player_Data, player_range):
+                print(formatted_string(i))
+            pass
 
 
 execute()
